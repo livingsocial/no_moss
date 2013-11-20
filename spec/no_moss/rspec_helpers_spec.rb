55438ee057b9baf0f9613eb43101f4fcb850f8c3
Sam Livingston-Gray <geeksam@gmail.com>
2013-11-19 19:05:23 -0800

    Add spec for failure message of custom matcher

--- spec/no_moss/rspec_helpers_spec.rb
+++ spec/no_moss/rspec_helpers_spec.rb
@@ -38,8 +38,15 @@ describe 'RSpec helpers' do
       expect(yak).to play_role(Agriculture::Roles::Cattle)
     end
 
-    it 'should know when an object does not play a role' do
-      expect(cat).to_not play_role(Agriculture::Roles::Cattle)
+    it 'should have a reasonable error message when an object does not play a role' do
+      caught_exception = nil
+      begin
+        expect(cat).to play_role(Agriculture::Roles::Cattle)
+      rescue RSpec::Expectations::ExpectationNotMetError => e
+        caught_exception = e
+      end
+      expect(caught_exception.message).to include("to play the role of Agriculture::Roles::Cattle")
+      expect(caught_exception.message).to include("but it is missing the following method(s): moo, low, grunt")
     end
   end
 
